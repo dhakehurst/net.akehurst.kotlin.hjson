9a306e07ed69cf1af4bd5e06f04c0c750c24e752
David Akehurst <david.akehurst@itemis.de>
2023-06-22 07:52:53 +0200

    version bump to kotlin 1.9.0-RC

--- net.akehurst.kotlin.hjson/build.gradle.kts
+++ net.akehurst.kotlin.hjson/build.gradle.kts
@@ -19,14 +19,14 @@ import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import java.io.File
 
 plugins {
-    kotlin("multiplatform") version ("1.8.21") apply false
-    id("org.jetbrains.dokka") version ("1.8.10") apply false
+    kotlin("multiplatform") version ("1.9.0-RC") apply false
+    id("org.jetbrains.dokka") version ("1.8.20") apply false
     id("com.github.gmazzo.buildconfig") version("3.1.0") apply false
     id("nu.studer.credentials") version ("3.0")
-    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version("1.8.21") apply false
+    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version("1.9.0-RC") apply false
 }
-val kotlin_languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8
-val kotlin_apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8
+val kotlin_languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9
+val kotlin_apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9
 val jvmTargetVersion = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
 
 allprojects {
@@ -107,8 +107,8 @@ subprojects {
         sign(publishing.publications)
     }
 
-    rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-        rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().version = "1.22.19"
-    }
+   // rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+   //     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().version = "1.22.19"
+   // }
 
 }
